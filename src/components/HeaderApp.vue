79576c97af631a780f2b4eed371e92c79470dcd2
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-18 16:42:48 +0100

    header structure

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,17 @@
 <script>
+import NavBar from './header-components/NavBar.vue';
+
 export default {
-  
+  components:{
+    NavBar
+  }
 }
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <header class="bg-black text-white p-3">
+    <NavBar/>
+  </header>
 </template>
 
 <style lang="">
---
+++ src/components/header-components/NavBar.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <nav class="d-flex justify-content-between container">
+    <button>ORDER ONLINE</button>
+    <ul class="d-flex">
+      <li class="list-group-item">LINK</li>
+      <li class="list-group-item">LINK</li>
+      <li class="list-group-item">LINK</li>
+      <li class="list-group-item">img</li>
+      <li class="list-group-item">link</li>
+      <li class="list-group-item">link</li>
+      <li class="list-group-item">link</li>
+    </ul>
+    <ul class="d-flex">
+      <li class="list-group-item">link</li>
+      <li class="list-group-item">link</li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="">
+  
+</style>
