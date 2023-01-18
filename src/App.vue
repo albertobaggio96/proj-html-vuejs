7c7dec6274e5e8d740500fe5cbdfde989988122b
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-18 15:48:09 +0100

    add main components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import HeaderApp from './components/HeaderApp.vue';
+  import MainApp from './components/MainApp.vue';
+  import FooterApp from './components/FooterApp.vue';
+
+  export default{
+    components : {
+      HeaderApp,
+      MainApp,
+      FooterApp
+    }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HeaderApp/>
+
+  <MainApp/>
+
+  <FooterApp/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style >
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div>
+    Header 
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
