dba4d70a641cec05f7932186c53697922cce342a
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-18 17:25:43 +0100

    structure of advertising-images

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -4,16 +4,34 @@ import JumboApp from './main-components/JumboApp.vue';
 export default {
   components:{
     JumboApp
+  },
+  data(){
+    return{
+      advertisingImages: ["New Place", "Mangia una buona pizza", "Yum Elvis", "La pizza più grande"]
+    }
+  },
+  methods:{
+    getImgPath(imgPath){
+      return new URL(`../assets/img/h3-img-${imgPath + 1}.jpg`, import.meta.url).href;
+    },
   }
 }
 </script>
 
 <template>
   <main>
-    <JumboApp/>
+    <section id="jumbo" class="row">
+      <JumboApp/>
+    </section>
+
+    <section id="advertising-images" class="row">
+      <div class="col-lg-3 g-1" v-for="(alt, index) in advertisingImages">
+        <img class="img-fluid" :src="getImgPath(index)" :alt="alt">
+      </div>
+    </section>
   </main>
 </template>
 
-<style lang="">
+<style lang="scss">
   
 </style>
--- src/components/main-components/JumboApp.vue
+++ src/components/main-components/JumboApp.vue
@@ -6,11 +6,11 @@ export default {
 </script>
 
 <template>
-  <section class="d-flex justify-content-between py-3 bg-black">
+  <div class="col-lg-12 d-flex justify-content-between py-3 bg-black">
     <button>PREV</button>
     <img src="" alt="pizza">
     <button>NEXT</button>
-  </section>
+  </div>
 </template>
 
 <style lang="scss">
