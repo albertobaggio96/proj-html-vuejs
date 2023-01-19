860e08c557c2e6fb07c6f5920a995f1fd21866ba
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-19 08:46:31 +0100

    structure team-presentation

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,24 +1,51 @@
 <script>
 import JumboApp from './main-components/JumboApp.vue';
+import AdvertisingImages from "./main-components/AdvertisingImages.vue"
 import NewspapersReviews from './main-components/NewspapersReviews.vue'
 import PizzaSpecials from './main-components/PizzaSpecials.vue';
 import DontJudgePizza from './main-components/DontJudgePizza.vue'
+import TeamApp from './main-components/TeamApp.vue'
 
 export default {
-  components:{
+  components: {
     JumboApp,
+    AdvertisingImages,
     NewspapersReviews,
     PizzaSpecials,
     DontJudgePizza,
+    TeamApp,
   },
-  data(){
-    return{
-      advertisingImages: ["New Place", "Mangia una buona pizza", "Yum Elvis", "La pizza più grande"]
+  data() {
+    return {
+      advertisingImagesList: ["New Place", "Mangia una buona pizza", "Yum Elvis", "La pizza più grande"],
+      advertisingImagePath: "h3-img-",
+      teamPath: "h1-team-",
+      teamNames: [{
+        name: "Paolo Rossi",
+        path: "1a-700x700",
+        role: "Pizzaiolo"
+      },
+      {
+        name: "Francesca Zaia",
+        path: "2a",
+        role: "Waiter"
+      },
+      {
+        name: "Frank Bailey",
+        path: "4a",
+        role: "Kitchen Porter"
+      },
+      {
+        name: "Gioia Bellavia",
+        path: "3a",
+        role: "Manager"
+      },
+      ],
     }
   },
-  methods:{
-    getImgPath(imgPath){
-      return new URL(`../assets/img/h3-img-${imgPath + 1}.jpg`, import.meta.url).href;
+  methods: {
+    getImgPath(imgPathstart, imgPathEnd = "") {
+      return new URL(`../assets/img/${imgPathstart}${imgPathEnd}.jpg`, import.meta.url).href;
     },
   }
 }
@@ -27,29 +54,41 @@ export default {
 <template>
   <main>
     <section id="jumbo" class="row">
-      <JumboApp/>
+      <JumboApp />
     </section>
 
     <section id="advertising-images" class="row">
-      <div class="col-lg-3 g-1" v-for="(alt, index) in advertisingImages">
-        <img class="img-fluid" :src="getImgPath(index)" :alt="alt">
-      </div>
+      <article class="col-lg-3 g-1" v-for="(alt, index) in advertisingImagesList">
+        <img class="img-fluid" :src="getImgPath(advertisingImagePath, index + 1)" :alt="alt">
+      </article>
     </section>
 
     <section id="newspapers-reviews" class="row">
-      <NewspapersReviews/>
+      <NewspapersReviews />
     </section>
 
     <section id="pizza-specials" class="row">
-      <PizzaSpecials/>
+      <PizzaSpecials />
     </section>
 
     <section id="dont-judje-pizza" class="row">
-      <DontJudgePizza/>
+      <DontJudgePizza />
+    </section>
+
+    <section id="team-presentation" class="row">
+      <article class="col-lg-3 px-0" v-for="(person) in teamNames">
+        <img class="img-fluid" :src="getImgPath(teamPath, person.path)" :alt="person.name">
+      </article>
+    </section>
+
+    <section id="logos">
+      <div class="container">
+        <div class=""></div>
+      </div>
     </section>
   </main>
 </template>
 
 <style lang="scss">
-  
+
 </style>
